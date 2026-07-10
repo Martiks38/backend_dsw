9db4bdc979082be50f5be054fb3cfb5b06d64c35
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-07-10 02:07:18 -0300

    feat: add Swagger and bcrypt support

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Club Náutico')
+    .setDescription('API para la gestión del club náutico')
+    .setVersion('1.0')
+    .addTag('socios')
+    .addTag('embarcaciones')
+    .addTag('cursos')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
