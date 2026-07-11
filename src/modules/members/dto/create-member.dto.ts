d6d3789548476dfa8e8cca8d42780b223b09dfb3
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-07-11 03:18:51 -0300

    docs(members): configure swagger plugin and update create-member property

--- src/modules/members/dto/create-member.dto.ts
+++ src/modules/members/dto/create-member.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, Length, ValidateIf } from 'class-validator';
 
 export class CreateMemberDto {
@@ -27,14 +28,26 @@ export class CreateMemberDto {
   })
   documentNumber!: string;
 
+  @ApiProperty({
+    required: false,
+    description: 'Requerido si no se envía businessName',
+  })
   @ValidateIf((o: CreateMemberDto) => !o.businessName)
   @Length(2, 50, { message: 'El nombre debe tener entre 2 y 50 caracteres' })
   firstName?: string;
 
+  @ApiProperty({
+    required: false,
+    description: 'Requerido si no se envía businessName',
+  })
   @ValidateIf((o: CreateMemberDto) => !o.businessName)
   @Length(2, 50, { message: 'El apellido debe tener entre 2 y 50 caracteres' })
   lastName?: string;
 
+  @ApiProperty({
+    required: false,
+    description: ' Requerido si no se envían firstName y lastName',
+  })
   @ValidateIf((o: CreateMemberDto) => !o.firstName && !o.lastName)
   @Length(3, 100, {
     message: 'La razón social debe tener entre 3 y 100 caracteres',
