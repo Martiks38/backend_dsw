54420b4eecbd8448d9a81963251dce7e8bf79c71
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-07-20 19:13:09 -0300

    feat(auth): create roles guard

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+
+import { AuthenticatedUser } from '../authenticatedUser';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../role.enum';
+
+export interface AuthenticatedRequest extends Request {
+  user: AuthenticatedUser;
+}
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest<AuthenticatedRequest>();
+
+    return requiredRoles.some((role) => role === user.role);
+  }
+}
