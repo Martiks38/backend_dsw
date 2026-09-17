8794293731bb6e9297ebd2518da89e98ab1b9bbb
Giuliano Cappa <cappa.giuliano@gmail.com>
2026-09-17 01:30:35 -0300

    feat(app): add all resources to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './modules/auth/auth.module';
+import { BoatsModule } from './modules/boats/boats.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { EmployeesModule } from './modules/employees/employees.module';
 import { MailModule } from './modules/mail/mail.module';
 import { MembersModule } from './modules/members/members.module';
+import { ServiceRequestModule } from './modules/service-request/service-request.module';
+import { ServiceTypesModule } from './modules/service-types/service-types.module';
+import { UsersModule } from './modules/users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
@@ -18,6 +23,11 @@ import { PrismaModule } from './prisma/prisma.module';
     EmployeesModule,
     AuthModule,
     MailModule,
+    ServiceRequestModule,
+    DashboardModule,
+    BoatsModule,
+    ServiceTypesModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
